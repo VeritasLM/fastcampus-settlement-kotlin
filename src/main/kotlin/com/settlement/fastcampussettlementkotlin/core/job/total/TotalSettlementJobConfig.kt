854e3ec9dd4eeefc9f8ab14974a621cbfe5e9510
truekim819 <truekim819@gmail.com>
2023-10-09 03:53:35 +0900

    5-2 TotalSettlementReader만들기

---
+++ src/main/kotlin/com/settlement/fastcampussettlementkotlin/core/job/total/TotalSettlementJobConfig.kt
@@ -0,0 +1,43 @@
+package com.settlement.fastcampussettlementkotlin.core.job.total
+
+import com.settlement.fastcampussettlementkotlin.domain.entity.settlement.SettlementTotal
+import com.settlement.fastcampussettlementkotlin.domain.projection.SummingSettlementResponse
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.item.database.JpaPagingItemReader
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@Configuration
+@EnableTransactionManagement
+class TotalSettlementJobConfig(
+    private val jobRepository: JobRepository,
+    private val transactionManager: PlatformTransactionManager,
+    @Qualifier("totalSettlementJpaItemReader") private val totalSettlementJpaItemReader: JpaPagingItemReader<SummingSettlementResponse>,
+) {
+
+    private val JOB_NAME = "totalSettlementJob"
+    private val chunkSize = 500
+
+    @Bean
+    fun totalSettlementJob(): Job {
+        return JobBuilder(JOB_NAME, this.jobRepository)
+            .start(totalSettlementJobStep())
+            .build()
+    }
+
+    @Bean
+    fun totalSettlementJobStep(): Step {
+        return StepBuilder(JOB_NAME+"_step", this.jobRepository)
+            .chunk<SummingSettlementResponse, SettlementTotal>(this.chunkSize, this.transactionManager)
+            .reader(totalSettlementJpaItemReader)
+            .build()
+    }
+}
