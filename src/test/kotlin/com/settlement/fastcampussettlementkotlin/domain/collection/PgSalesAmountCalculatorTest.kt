caaddebe070d730dc79abdc82fe6e2af2da4c130
truekim819 <truekim819@gmail.com>
2023-10-16 05:33:37 +0900

    6-2 테스트 케이스 준비

--- src/test/kotlin/com/settlement/fastcampussettlementkotlin/domain/collection/PgSalesAmountCalculatorTest.kt
+++ src/test/kotlin/com/settlement/fastcampussettlementkotlin/domain/collection/PgSalesAmountCalculatorTest.kt
@@ -13,29 +13,29 @@ class PgSalesAmountCalculatorTest{
     @DisplayName("프로모션도 없고, 적립금 사용도 없는 경우 ")
     @Test
     fun test1() {
-        //val orderItemSnapshot = OrderItemSnapshot(
-        //    1,
-        //    sellerNo = 1,
-        //    productNo = 1,
-        //    sellPrice = BigDecimal.valueOf(10000.000),
-        //    supplyPrice = BigDecimal.valueOf(8000.000),
-        //    promotionAmount = BigDecimal.valueOf(0.000),
-        //    mileageUsageAmount = BigDecimal.ZERO,
-        //    seller = Seller(1,
-        //        "니모",
-        //        sellType = "P"),
-        //    product = Product(1, productName = "어항1", sellerNo = 1,
-        //        sellPrice = BigDecimal.valueOf(10000.000),
-        //        supplyPrice = BigDecimal.valueOf(8000.000),
-        //        category = 1),
-        //)
-
-        val pgSalesAmountMaterial = PgSalesAmountMaterial(
-            BigDecimal.valueOf(10000.000),
-            BigDecimal.valueOf(0.000),
-            BigDecimal.valueOf(0.000),
+        val orderItemSnapshot = OrderItemSnapshot(
+            1,
+            sellerNo = 1,
+            productNo = 1,
+            sellPrice = BigDecimal.valueOf(10000.000),
+            supplyPrice = BigDecimal.valueOf(8000.000),
+            promotionAmount = BigDecimal.valueOf(0.000),
+            mileageUsageAmount = BigDecimal.ZERO,
+            seller = Seller(1,
+                "니모",
+                sellType = "P"),
+            product = Product(1, productName = "어항1", sellerNo = 1,
+                sellPrice = BigDecimal.valueOf(10000.000),
+                supplyPrice = BigDecimal.valueOf(8000.000),
+                category = 1),
         )
-        val calculator = PgSalesAmountCalculator(pgSalesAmountMaterial)
+
+        //val pgSalesAmountMaterial = PgSalesAmountMaterial(
+        //    BigDecimal.valueOf(10000.000),
+        //    BigDecimal.valueOf(0.000),
+        //    BigDecimal.valueOf(0.000),
+        //)
+        val calculator = PgSalesAmountCalculator(orderItemSnapshot)
         val pgSalesAmount = calculator.getPgSaleAmount()
 
         Assertions.assertEquals(pgSalesAmount, BigDecimal.valueOf(10000.0))
